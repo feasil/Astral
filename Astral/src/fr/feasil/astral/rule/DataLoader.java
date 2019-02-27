2a367b2c6e012a7f998f84e9ac99a6c9528a4467
feasil <feasil@hotmail.fr>
2019-02-27 18:22:31 +0100

    Multiple Data

--- Astral/src/fr/feasil/astral/rule/DataLoader.java
+++ Astral/src/fr/feasil/astral/rule/DataLoader.java
@@ -7,11 +7,13 @@ import fr.feasil.astral.theme.ruleengine.ActionDispatcher;
 import fr.feasil.astral.theme.ruleengine.ExpressionParser;
 import fr.feasil.astral.theme.ruleengine.Rule;
 import fr.feasil.astral.theme.ruleengine.Rules;
+import fr.feasil.astral.theme.ruleengine.expression.Operation;
+import fr.feasil.astral.theme.ruleengine.expression.Operations;
 import fr.feasil.utils.xlsloader.ExcelFile;
 import fr.feasil.utils.xlsloader.ExcelSheet;
 
 public class DataLoader {
-	private final static String DATA_SHEET = "Data";
+//	private final static String DATA_SHEET = "Data";
 	
 	private ExcelFile file;
 	
@@ -29,30 +31,39 @@ public class DataLoader {
 	
 	
 	private void loadData() {
-		ExcelSheet sheet = file.getSheet(DATA_SHEET);
-		List<String> headers = new ArrayList<>();
-		headers.add(null);
-		for ( int i = 1 ; i < sheet.getHeader().length ; i++ )
-			headers.add(sheet.getHeader()[i].replaceAll(" ", ""));
-		
-		for ( Object[] ligne : sheet.getBody() )
-		{
-			String rowName = ligne[0].toString().replaceAll(" ", "");
-			for ( int i = 1 ; i < ligne.length ; i++ ) {
-				if ( ligne[i] != null )
-				{
-					String expression = rowName + " EN " + headers.get(i);
-					String content = ligne[i].toString();
-					rules.addRule(new Rule.Builder()
-							.withExpression(ExpressionParser.fromString(expression))
-							.withDispatcher(new ActionDispatcher() {
-								@Override
-								public void fire() {
-									//TODO trouver une solution pour ça !
-									System.out.println(content);
-								}
-							})
-							.build());
+		for (ExcelSheet sheet : file.getListSheets() ) {
+			Operation op = null;
+			if (  sheet.getHeader().length > 0 && sheet.getHeader()[0] != null )
+				op = Operations.INSTANCE.getOperation(sheet.getHeader()[0]);
+			if ( op == null)
+				op = Operations.INSTANCE.getOperation("EN");
+			String operationSymbol = " " + op.getSymbol() + " ";
+			
+//			ExcelSheet sheet = file.getSheet(DATA_SHEET);
+			List<String> headers = new ArrayList<>();
+			headers.add(null);
+			for ( int i = 1 ; i < sheet.getHeader().length ; i++ )
+				headers.add(sheet.getHeader()[i].replaceAll(" ", ""));
+			
+			for ( Object[] ligne : sheet.getBody() )
+			{
+				String rowName = ligne[0].toString().replaceAll(" ", "");
+				for ( int i = 1 ; i < ligne.length ; i++ ) {
+					if ( ligne[i] != null )
+					{
+						String expression = rowName + operationSymbol + headers.get(i);
+						String content = ligne[i].toString();
+						rules.addRule(new Rule.Builder()
+								.withExpression(ExpressionParser.fromString(expression))
+								.withDispatcher(new ActionDispatcher() {
+									@Override
+									public void fire() {
+										//TODO trouver une solution pour ça !
+										System.out.println(content);
+									}
+								})
+								.build());
+					}
 				}
 			}
 		}
--- Astral/src/fr/feasil/astral/theme/ruleengine/Rules.java
+++ Astral/src/fr/feasil/astral/theme/ruleengine/Rules.java
@@ -2,9 +2,11 @@ package fr.feasil.astral.theme.ruleengine;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import fr.feasil.astral.theme.Theme;
 
+/**
+ * https://stackoverflow.com/questions/20763189/creating-a-simple-rule-engine-in-java
+ */
 public class Rules {
 	private List<Rule> rules = new ArrayList<>();
 
